542cb2e16002b11e5e46a200bf7f98f9ed371cce
lipho <hello@lipho.net>
2021-03-27 17:51:36 -0700

    Add Loader, add styles, add renderContent helper

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,33 @@
+import './SeasonDisplay.css';
+
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  }
+  return lat > 0 ? 'winter' : 'summer';
+}
+
+const seasonConf = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    icon: 'sun',
+  },
+  winter: {
+    text: 'Burr, it\'s chilly',
+    icon: 'snowflake',
+  }
+}
+
 const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, icon} = seasonConf[season];
   return (
-    <div>
-      Season Display
+    <div className={`season-display ${season}`}>
+      <i className={`${icon} icon massive icon-left`}/>
+      <h1>{text}</h1>
+      <i className={`${icon} icon massive icon-right`}/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SeasonDisplay from "./SeasonDisplay";
+import "semantic-ui-css/semantic.min.css";
 
-const App = () => {
-  return <SeasonDisplay/>
+
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
+
+class App extends React.Component {
+  state = {lat: null, errMsg: null};
+
+  componentDidMount() {
+
+    window.navigator.geolocation.getCurrentPosition(
+      pos => this.setState({lat: pos.coords.latitude}),
+      err => this.setState({errMsg: err.message}),
+    );
+
+  }
+
+
+  renderContent() {
+    if (this.state.errMsg) {
+      return <div>ErrorMessage: {this.state.errMsg}</div>
+    }
+    if (this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+    return <Spinner/>
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderContent()}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
